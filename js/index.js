1cfb08a2778976973791983d52a5f40a4bad80e8
Jessica Erazo <jessicamerazo@gmail.com>
2018-05-26 17:26:35 -0700

    restyled app, cleaned up code, added wow.js animations, fixed reponsive issues

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const GITHUB_ENDPOINT = 'https://api.github.com/search/repositories?per_page=10&page=1&q=sketch%20plugin';
-const TWITTER_SEARCH_ENDPOINT = 'https://api.twitter.com/1.1/search/tweets.json?q=sketch%20plugins&result_type=recent&count=3&lang=en';
+const TWITTER_SEARCH_ENDPOINT = 'https://api.twitter.com/1.1/search/tweets.json?q=sketch%20plugin&result_type=recent&count=3&lang=en';
 const CORS_ANYWHERE_ENDPOINT = 'https://cors-anywhere.herokuapp.com/';
+
 
 function renderTwitterResult(result) {
   /*  The image coming back from Twitter API was http
@@ -13,8 +14,8 @@ function renderTwitterResult(result) {
   let profileUrlStr = result.user.profile_image_url;
   let newProfileUrlStr = profileUrlStr.replace("http", "https");
   return `
-        <div class="twitter-result-box row js-twitter-result-box">
-        <a href="https://twitter.com/${result.user.screen_name}" target="blank" class="profile-url">
+        <div class="twitter-result-box row js-twitter-result-box wow fadeInLeftBig" data-wow-duration="2.5s" data-wow-delay="1"">
+        <a href="https://twitter.com/${result.user.screen_name}" target="blank">
             <img src="${newProfileUrlStr}" alt="profile image of${result.user.screen_name} " class="avatar">
         </a>
          <div class="profile-image-container"></div>
@@ -23,7 +24,7 @@ function renderTwitterResult(result) {
                     <a href="https://twitter.com/${result.user.screen_name}" target="blank" class="profile-url">${result.user.name}</a>
                 </h3>
                 <p class="tweet-msg">
-                    <a href="https://twitter.com/${result.user.screen_name}" target="blank" class="profile-url">${result.text}</a>
+                    <a href="https://twitter.com/${result.user.screen_name}" target="blank" class="profile-msg">${result.text}</a>
                 </p>
             </div>
         </div>`;
@@ -49,14 +50,19 @@ function getTwitterDataFromAPI(callback) {
 function renderResult(result) {
   const downloadLink = result.html_url + "/archive/master.zip";
   return `
-        <div class="result-box">
+        <div class="result-box wow fadeIn" data-wow-duration="2s" data-wow-delay="0">
+          <div class="result-container">
                 <h2 class="sketch-title">
                     <a href="${result.html_url}" target="blank">${result.name}</a>
                 </h2>
                 <p class="sketch-description">${result.description}</p>
-                <p>
-                    <a href="${downloadLink}" class="download-link" aria-label="download-plugin">Download</a>
-                </p>
+                <div class="download-btn">
+                    <a href="${downloadLink}" aria-label="download-plugin" class="">
+                      <img src="css/images/cloud-icon@2x.png" alt="cloud download" class="cloud-icon">
+                        Download
+                    </a>
+                </div>
+          </div>
         </div>`;
 }
 
@@ -65,7 +71,7 @@ function displayGitHubSearchData(data) {
   if (data.total_count !== 0) {
     $('.js-search-results').html(results);
   } else {
-    $('.js-search-results').html("<p class=\"fail-statement\">Sorry, no results were found. Please try searching again.</p>");
+    $('.js-search-results').html(`<p class="fail-statement wow swing" data-wow-duration="2s" data-wow-delay="0s">Sorry, no results were found. Please try searching again.</p>`);
   }
 }
 
